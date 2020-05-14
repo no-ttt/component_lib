8a184cebec3846ec99a402ffd8eceae5f7d4f73e
hjhsu <hjhsu@wke.tw>
2020-05-14 11:35:16 +0800

    新增 環境css

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,9 +1,9 @@
 const pkg = require('./package.json');
 const path = require('path');
 module.exports = {
-  title: `Chart Component v${pkg.version}`,
-  components: 'src/*/*.js',
-  
+  title: `${pkg.name} v${pkg.version}`,
+  components: 'src/*/*.jsx',
+
   webpackConfig: {
     module: {
       rules: [
@@ -11,13 +11,18 @@ module.exports = {
           test: /\.jsx?$/,
           exclude: /node_modules/,
           loader: 'babel-loader'
+        }, {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
         }
       ]
     }
   },
-  
+  require: [
+    path.join(__dirname, 'public/w3.css')
+  ],
   getComponentPathLine(componentPath) {
-    const name = path.basename(componentPath, '.js');
+    const name = path.basename(componentPath, '.jsx');
 
     return `import { ${name} } from '${pkg.name}';`;
   },
