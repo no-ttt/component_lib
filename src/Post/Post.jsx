ded1ea2d0336dadbb15769cad182543cfa69036a
no-ttt <dear890329@gmail.com>
2022-02-08 15:19:46 +0800

    post

--- src/Post/Post.jsx
+++ src/Post/Post.jsx
@@ -6,27 +6,33 @@ import LocationOnIcon from '@material-ui/icons/LocationOn'
 
 export class Post extends Component {
 	static propTypes = {
-		/** 景點圖片說明 */
-		alt: PropTypes.string,
+		/** 景點圖片名稱 */
+		title: PropTypes.string.isRequired,
 		/** 景點圖片連結網址 */
 		src: PropTypes.string.isRequired,
 		/** 景點位置 */
-		addr: PropTypes.string.isRequired
+    addr: PropTypes.string.isRequired,
+    /** 圖片寬度 */
+    width: PropTypes.number,
+    /** 圖片高度 */
+    height: PropTypes.number,
 	}
 
 	static defaultProps = {
+    width: 280,
+    height: 220,
 	}
 
 	render() {
-		const { post } = this.props;
+		const { post, width, height } = this.props;
 		let allPost = [];
 
 		for (let i = 0; i < post.length; i++) {
 			allPost.push (
-				<div>
-					<img title={post[i].alt} src={post[i].src} alt={post[i].alt} className="post-image"></img>
+        <div>
+          <img title={post[i].title} src={post[i].src} alt={post[i].alt} width={width} height={height} className="post-image"></img>
 					<div className="post-des">
-						<div className="post-name">{post[i].alt}</div>
+						<div className="post-name">{post[i].title}</div>
 						<div className="post-addr">
 							<LocationOnIcon color="action" fontSize="small" />
 							<div className="post-addr-txt">{post[i].addr}</div>
@@ -37,7 +43,7 @@ export class Post extends Component {
 		}
 
 		return (
-			<div>{post}</div>
+			<div className="post-sort">{allPost}</div>
 		)
 	}
 }
--- src/Post/Post.md
+++ src/Post/Post.md
@@ -1,10 +1,11 @@
 ```jsx
   let post = [
-		{ alt: "日月潭", src: "https://images.chinatimes.com/newsphoto/2021-07-17/656/20210717002224.jpg", addr: "南投" },
-		{ alt: "鹿野高台", src: "https://media.vogue.com.tw/photos/5eba4d64f9d3ef21ac84e703/16:9/w_1620,h_911,c_limit/2018%E7%86%B1%E6%B0%A3%E7%90%83%E5%98%89%E5%B9%B4%E8%8F%AF.jpg", addr: "台東"},
-	];
+    { title: "日月潭", src: "https://images.chinatimes.com/newsphoto/2021-07-17/656/20210717002224.jpg", addr: "南投" },
+    { title: "鹿野高台", src: "https://cdn2.ettoday.net/images/4995/d4995352.jpg", addr: "台東"},
+    { title: "愛河", src: "https://pic.pimg.tw/anrine910070/1602321228-4026170466-g.jpg", addr: "高雄" },
+  ];
 
-	<div style={{ display: "flex", justifyContent: "space-around" }}>
-    <Post post={post} />
-	</div>
+  <div>
+    <Post post={post} width={280} height={220} />
+  </div>
 ```
