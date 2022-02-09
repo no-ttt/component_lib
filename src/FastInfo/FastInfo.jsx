c8288247e01c1c76248715d95f297f232719f7f6
no-ttt <dear890329@gmail.com>
2022-02-09 13:06:37 +0800

    景點簡介

--- src/FastInfo/FastInfo.jsx
+++ src/FastInfo/FastInfo.jsx
@@ -23,8 +23,10 @@ export class FastInfo extends Component {
 		remark: PropTypes.string,
 		/** 交通方式 */
 		traffic: PropTypes.string,
-		/** 座標 */
-		pos: PropTypes.string,
+		/** 緯度 */
+		posLat: PropTypes.number,
+		/** 經度 */
+		posLon: PropTypes.number,
 	}
 
 	static defaultProps = {
@@ -32,10 +34,54 @@ export class FastInfo extends Component {
 	}
 
 	render() {
-		const { spotName, des, phone, addr, picture, open, ticket, remark, traffic, pos } = this.props;
+		const { spotName, des, phone, addr, picture, open, ticket, remark, traffic, posLat, posLon } = this.props;
 		return (
-			<div>
-			  
+			<div className="fastInfo-frame">
+				<div className="fastInfo-des">
+					<img src={picture} width="300" height="200" className="fastInfo-image"></img>
+					<div className="fastInfo-des-distance">
+						<div className="fastInfo-spotName-txt">{spotName}</div>
+						<table className="fastInfo-table">
+							<tbody>
+								<tr>
+									<td valign="top" className="fastInfo-td">簡介</td>
+									<td>{des}</td>
+								</tr>
+								<tr>
+									<td valign="top" className="fastInfo-td">電話</td>
+									<td>{phone}</td>
+								</tr>
+								<tr>
+									<td valign="top" className="fastInfo-td">地址</td>
+									<td>
+										<a href={"https://www.google.com.tw/maps/@"+posLat.toFixed(7).toString()+","+posLon.toFixed(7).toString()+",15z"} target="_blank" >{addr}</a>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+				<div className="fastInfo-other">
+					<table className="fastInfo-other-table">
+						<tbody>
+							<tr>
+								<td valign="top" className="fastInfo-other-td">開放時間</td>
+								<td>{open}</td>
+							</tr>
+							<tr>
+								<td valign="top" className="fastInfo-other-td">門票資訊</td>
+								<td>{ticket}</td>
+							</tr>						<tr>
+								<td valign="top" className="fastInfo-other-td">注意事項</td>
+								<td>{remark}</td>
+							</tr>
+							<tr>
+								<td valign="top" className="fastInfo-other-td">交通方式</td>
+								<td>{traffic}</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>	
 			</div>
 		)
 	}
