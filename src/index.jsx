fda4f29fe561b32f58400f75ba9b9f7331dd2d08
no-ttt <dear890329@gmail.com>
2022-02-09 17:21:03 +0800

    update index

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
 export {default as Avatar} from "./Avatar/Avatar";
 export {default as NavBar} from "./NavBar/NavBar";
 export {default as Post} from "./Post/Post";
-export {default as PhotoWall} from "./PhotoWall/PhotoWall";
+export {default as FastInfo} from "./FastInfo/FastInfo";
+export {default as TravelCard} from "./TravelCard/TravelCard";
