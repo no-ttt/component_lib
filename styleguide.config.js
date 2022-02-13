30a866c1648dcd428d2b433bce6ac69031f57370
hjhsu <hjhsu@wke.tw>
2022-02-13 19:26:05 +0800

    add style

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,55 +1,55 @@
-const pkg = require('./package.json');
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
+const pkg = require("./package.json")
+const path = require("path")
+const CopyPlugin = require("copy-webpack-plugin")
 module.exports = {
-  title: `${pkg.name} v${pkg.version}`,
-  components: 'src/*/*.jsx',
+	title: `${pkg.name} v${pkg.version}`,
+	components: "src/*/*.jsx",
 
-  webpackConfig: {
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader'
-        }, {
-          test: /\.css$/,
-          use: ["style-loader", "css-loader"]
-        }, {
-          test: /\.styl?/,
-          use: ["style-loader", "css-loader", "stylus-loader"]
-        }, {
-          test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-          loader: 'url-loader',
-          options: {
-            limit: 10000
-          }
-        }
-      ]
-    },
-    plugins: [
-      new CopyPlugin({
-        patterns: [
-          { from: path.join(__dirname, 'src/style'), to: path.join(__dirname, 'dist/style') },
-        ]
-      }),
-    ],
-  },
-  template: {
-    head: {
-      links: [
-        {
-          rel: 'stylesheet',
-          href:
-            'https://use.fontawesome.com/releases/v5.4.1/css/all.css'
-        },
-      ]
-    }
-  },
-  require: [],
-  getComponentPathLine(componentPath) {
-    const name = path.basename(componentPath, '.jsx');
+	webpackConfig: {
+		module: {
+			rules: [
+				{
+					test: /\.jsx?$/,
+					exclude: /node_modules/,
+					loader: "babel-loader",
+				},
+				{
+					test: /\.css$/,
+					use: ["style-loader", "css-loader"],
+				},
+				{
+					test: /\.styl?/,
+					use: ["style-loader", "css-loader", "stylus-loader"],
+				},
+				{
+					test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+					loader: "url-loader",
+					options: {
+						limit: 10000,
+					},
+				},
+			],
+		},
+		plugins: [
+			new CopyPlugin({
+				patterns: [{ from: path.join(__dirname, "src/style"), to: path.join(__dirname, "dist/style") }],
+			}),
+		],
+	},
+	template: {
+		head: {
+			links: [
+				{
+					rel: "stylesheet",
+					href: "https://use.fontawesome.com/releases/v5.4.1/css/all.css",
+				},
+			],
+		},
+	},
+	require: [path.join(__dirname, "style/styles.styl")],
+	getComponentPathLine(componentPath) {
+		const name = path.basename(componentPath, ".jsx")
 
-    return `import { ${name} } from '${pkg.name}';`;
-  },
-};
+		return `import { ${name} } from '${pkg.name}';`
+	},
+}
