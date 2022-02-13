ddfacd18fdaf6b2886fcd82f96d36ae4c07ad348
hjhsu <hjhsu@wke.tw>
2022-02-13 18:57:22 +0800

    'deploy'

---
+++ dist/index.js
@@ -0,0 +1,1 @@
+"use strict";var _Example=_interopRequireDefault(require("./Example/Example"));Object.defineProperty(exports,"__esModule",{value:!0}),Object.defineProperty(exports,"Example",{enumerable:!0,get:function get(){return _Example["default"]}});function _interopRequireDefault(a){return a&&a.__esModule?a:{default:a}}
