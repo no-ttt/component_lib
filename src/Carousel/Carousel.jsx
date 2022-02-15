26efc119ac8915e0a5430dd2eacdefd6909bfab8
no-ttt <dear890329@gmail.com>
2022-02-16 02:43:25 +0800

    圖片輪播 css 設定

--- src/Carousel/Carousel.jsx
+++ src/Carousel/Carousel.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import classNames from "classnames"
 import PropTypes from 'prop-types'
 import CSSModules from 'react-css-modules'
 import styles from '../style/Carousel.styl'
 import Post from '../Post/Post'
+import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft'
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
 
 export class Carousel extends Component {
 	static propTypes = {
@@ -43,13 +44,13 @@ export class Carousel extends Component {
 		const itemSetList = post.reduce((result, item, i) => {
 			if (i % cols === 0) {
 				result.push([
-					<div key={i} className="item">
+					<div key={i}>
 						<Post title={item.title} src={item.src} width={width} height={height} fontSize={fontSize} />
 					</div>
 				])
 			} else {
 				result[result.length - 1].push(
-					<div key={i} className="item">
+					<div key={i}>
 						<Post title={item.title} src={item.src} width={width} height={height} fontSize={fontSize} />
 					</div>
 				)
@@ -72,44 +73,47 @@ export class Carousel extends Component {
 			})
 		}
 
-
 		return (
 			<div className="Carousel">
-				<span 
-					className="Carousel_btn--prev" 
-					hidden={this.state.currentPage <= 0} 
-					onClick={handlePrev} 
-				/>
-
+				<div hidden={this.state.currentPage <= 0} >
+					<button 
+						onClick={handlePrev} 
+						className="Carousel_btn"
+					>
+						<KeyboardArrowLeftIcon />
+					</button>
+				</div>
 				<div className="Carousel_railWrapper">
-        <div
-          className="Carousel_rail"
-          style={{
-            gridTemplateColumns: `repeat(${page}, 100%)`,
-            left: `calc(${-100 * this.state.currentPage}% - ${gap * this.state.currentPage}px)`,
-            gridColumnGap: `${gap}px`
-          }}
-        >
-          {itemSetList.map((set, i) => (
-            <div
-              key={i}
-              style={{
-                display: 'grid',
-                gridTemplateColumns: `repeat(${cols}, 1fr)`,
-                gridGap: `${gap}px`
-              }}
-            >
-              {set}
-            </div>
-          ))}
-        </div>
-      </div>
-				
-				<span 
-					className="Carousel_btn--next" 
-					hidden={this.state.currentPage=== page - 1} 
-					onClick={handleNext} 
-				/>
+					<div
+						className="Carousel_rail"
+						style={{
+							gridTemplateColumns: `repeat(${page}, 100%)`,
+							left: `calc(${-100 * this.state.currentPage}% - ${gap * this.state.currentPage}px)`,
+							gridColumnGap: `${gap}px`
+						}}
+					>
+						{itemSetList.map((set, i) => (
+							<div
+								key={i}
+								style={{
+									display: 'grid',
+									gridTemplateColumns: `repeat(${cols}, 1fr)`,
+									gridGap: `${gap}px`
+								}}
+							>
+								{set}
+							</div>
+						))}
+        	</div>
+      	</div>
+				<div hidden={this.state.currentPage=== page - 1} >
+					<button
+						className="Carousel_btn"
+						onClick={handleNext} 
+					>
+						<KeyboardArrowRightIcon />
+					</button>
+				</div>
 			</div>
 		)
 	}
