fad8e0f851ef320151bc1ff6a679e7b0480585bf
no-ttt <dear890329@gmail.com>
2022-02-15 16:21:01 +0800

    index

--- src/index.jsx
+++ src/index.jsx
@@ -3,3 +3,5 @@ export {default as NavBar} from "./NavBar/NavBar";
 export {default as Post} from "./Post/Post";
 export {default as FastInfo} from "./FastInfo/FastInfo";
 export {default as TravelCard} from "./TravelCard/TravelCard";
+export {default as Carousel} from "./Carousel/Carousel";
+export {default as SearchBox} from "./SearchBox/SearchBox";
