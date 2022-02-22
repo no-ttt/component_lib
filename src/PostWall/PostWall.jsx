548155b6d497e260e63f9e4b9c113ddc817da753
no-ttt <dear890329@gmail.com>
2022-02-22 15:14:16 +0800

    首頁貼文牆

--- src/PostWall/PostWall.jsx
+++ src/PostWall/PostWall.jsx
@@ -7,20 +7,48 @@ export class PostWall extends Component {
 	static propTypes = {
 		/** 列的貼文數量*/
 		cols: PropTypes.number,
-		/** 行的貼文數量 */
-		rowa: PropTypes.number,
+		/** 間距 */
+		gap: PropTypes.number,
 	}
 
 	static defaultProps = {
-		rows: 2,
 		cols: 3,
+		gap: 20,
 	}
 
 	render() {
-		const {  } = this.props;
+		const { cols, gap, post } = this.props;
 		return (
-			<div>
-				
+			<div style={{
+				display: 'grid', 
+				gridColumnGap: `${gap}px`, 
+				gridRowGap: `${gap}px`, 
+				gridTemplateColumns: `repeat(${cols}, 1fr)`
+			}}>
+				{post.map((t) => 
+					<div className="postwall-layout">
+						<div className="postwall-title">{t.title}</div>
+						<div className="postwall-des-layout">
+							<div>
+								<span className="postwall-des" style={{fontSize: 20}}>{t.day}</span>
+								<span>&nbsp;</span>
+								<span className="postwall-des" style={{fontSize: 14}}>DAYS</span>
+							</div>
+							<div>
+								<span className="postwall-des" style={{fontSize: 20}}>{t.like}</span>
+								<span>&nbsp;</span>
+								<span className="postwall-des" style={{fontSize: 14}}>LIKES</span>
+							</div>
+						</div>
+						<div className="postwall-img-overfit">
+							<div className="postwall-cover">
+								<img title={t.title} src={t.src} alt={t.tile} className="postwall-img"></img>
+							</div>
+						</div>
+
+						
+					</div>
+				)}
 			</div>
 		)
 	}
--- src/PostWall/PostWall.md
+++ src/PostWall/PostWall.md
@@ -1,5 +1,14 @@
 ```jsx
+  let post = [
+    { title: "日月潭划船", src: "https://images.chinatimes.com/newsphoto/2021-07-17/656/20210717002224.jpg", day: "2", like: "20" },
+    { title: "古蹟之旅", src: "http://b.blog.xuite.net/b/2/d/e/12584724/blog_32120/txt/33774123/56.jpg", day: "3", like: "10" },
+    { title: "熱氣球嘉年華", src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDeeaPEpQk4HgDPIAEQ3rJFRKwHo97L-07iA&usqp=CAU", day: "2", like: "50" },
+    { title: "新北耶誕城", src: "https://imgs.gvm.com.tw/upload/gallery/20201114/75871_01.jpg", day: "2", like: "12" },
+    { title: "魚你相遇", src: "https://www.viviantrip.com/wp-content/uploads/2021/03/%E6%B5%B7%E7%94%9F%E9%A4%A8-4.jpg", day: "1", like: "8" },
+    { title: "親自大自然", src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR813SKMm9B_vyUViP54He3z4u92UGXC9S3PA&usqp=CAU", day: "4", like: "21" },
+	];
+	
   <div>
-    <PostWall cols={3} rows={2} />
+    <PostWall cols={3} gap={20} post={post} />
   </div>
 ```
