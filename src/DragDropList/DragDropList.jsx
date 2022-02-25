dabd18affd6e6eb2bcd29ab3bae2741265e62396
no-ttt <dear890329@gmail.com>
2022-02-25 15:24:19 +0800

    行程規劃拖移及刪除

--- src/DragDropList/DragDropList.jsx
+++ src/DragDropList/DragDropList.jsx
@@ -1,54 +1,88 @@
-import React, { useState, useRef } from 'react';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import styles from '../style/DragDropList.styl'
+import DeleteIcon from '@material-ui/icons/Delete'
 
+export class DragDropList extends Component {
+	static propTypes = {
+		/** 所有景點 */
+		listItem: PropTypes.array.isRequired,
+		/** 框的寬度 */
+		width: PropTypes.number,
+		/** 框的高度 */
+		height: PropTypes.number,
+	}
 
-export default function DragDropList(){
-  const draggingItem = useRef();
-  const dragOverItem = useRef();
+	static defaultProps = {
+		width: 250,
+		height: 60,
+	}
 
-  const [list, setList] = useState([
-    'Chicken Biriyani',
-    'Mutton Pulao',
-    'Paneer Masala',
-    'Tandoori Chicken',
-    'Dal Makhana',
-    'Malai Kofta',
-  ]);
+	constructor(props) {
+		super(props);
+		this.state = {
+			list: this.props.listItem
+		}
+		// 正在拖移的元素
+		this.draggingItem = React.createRef();
+		// 被取代位置的元素
+		this.dragOverItem = React.createRef();
+		this.handleDragStart = this.handleDragStart.bind(this);
+		this.handleDragEnter = this.handleDragEnter.bind(this);
+	}
 
-	const handleDragStart = (e, position) => {
-		draggingItem.current = position;
-		console.log(e.target.innerHTML); 
-	};
+	handleDragStart = (e, position) => {
+		this.draggingItem.current = position;
+		console.log("handleDragStart: " + e.target.innerHTML);
+	}
 
-	const handleDragEnter = (e, position) => {
-    dragOverItem.current = position;
-    console.log(e.target.innerHTML);
-    const listCopy = [...list];
-    console.log(draggingItem.current, dragOverItem.current);
-    const draggingItemContent = listCopy[draggingItem.current];
-    listCopy.splice(draggingItem.current, 1);
-    listCopy.splice(dragOverItem.current, 0, draggingItemContent);
+	handleDragEnter = (e, position) => {
+		this.dragOverItem.current = position;
+		console.log("handleDragEnter: " + e.target.innerHTML);
+		const listCopy = [...this.state.list];
+		console.log("dragging: " + this.draggingItem.current, "drag over: " + this.dragOverItem.current);
+		// 將正在拖移的元素存入 draggingItemContent
+		const draggingItemContent = listCopy[this.draggingItem.current];
+		// 從 list 刪除正在拖移的元素
+		listCopy.splice(this.draggingItem.current, 1);
+		// 將 正在拖移的元素(draggingItemContent) 加到要移入的位置
+		listCopy.splice(this.dragOverItem.current, 0, draggingItemContent);
 
-    draggingItem.current = dragOverItem.current;
-    dragOverItem.current = null;
-    setList(listCopy);
-  };
+		this.draggingItem.current = this.dragOverItem.current;
+		this.dragOverItem.current = null;
 
-  return (
-		<>
-		{
-			list &&
-			list.map((item, index) => (
-				<h1 
-					onDragStart={(e) => handleDragStart(e, index)}
-					onDragOver={(e) => e.preventDefault()}
-					onDragEnter={(e) => handleDragEnter(e, index)}
-					key={index}
-					draggable
-				>
-					{item}
-				</h1>
+		this.setState({
+			list: listCopy
+		})
+	}
+
+	render() {
+		const { width, height } = this.props;
+		return (
+			<div>
+				{this.state.list.map((item, index) => (
+          <div
+            onDragStart={(e) => this.handleDragStart(e, index)}
+            onDragOver={(e) => e.preventDefault()}
+            onDragEnter={(e) => this.handleDragEnter(e, index)}
+            key={index}
+						draggable
+						className="dragdroplist-frame"
+						style={{width: width, height: height}}
+					>
+						<div className="dragdroplist-layout">
+							<img title={item.title} src={item.src} alt={item.title} className="dragdroplist-img-frame"></img>
+							<div>{item.title}</div>
+						</div>
+						<button className="dragdroplist-btn" 
+							onClick={() => this.setState({ list: this.state.list.filter(listItem => listItem !== item)})}
+						>
+							<DeleteIcon />
+						</button>
+					</div>
 				))}
-			</>
-	);
+			</div>
+		)
+	}
+
 }
