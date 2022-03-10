c2e41bc5473d8519e6365655721e7e8ba237ce5e
no-ttt <dear890329@gmail.com>
2022-03-10 14:16:37 +0800

    未完成的tab

---
+++ src/Tabs/Tabs.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import CSSModules from 'react-css-modules'
+import styles from '../style/Tab.styl'
+
+export class Tab extends Component {
+	static propTypes = {
+
+	}
+
+	static defaultProps = {
+
+	}
+
+	render() {
+		const {  } = this.props;
+		return (
+			<div></div>
+		)
+	}
+}
+
+export default CSSModules(Tabs, styles)
