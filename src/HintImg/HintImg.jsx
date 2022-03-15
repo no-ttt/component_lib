fc9bea11b8b8a3b47e1f66cab70404c9e579bd4e
no-ttt <dear890329@gmail.com>
2022-03-15 13:59:07 +0800

    圖片帶有特效提示字

--- src/HintImg/HintImg.jsx
+++ src/HintImg/HintImg.jsx
@@ -5,22 +5,18 @@ import styles from '../style/HintImg.styl'
 
 export class HintImg extends Component {
 	static propTypes = {
-
-	}
-
-	static defaultProps = {
-
+		/** 底層圖片 */
+		src: PropTypes.string.isRequired,
+		/** 內容文字 */
+		content: PropTypes.string,
 	}
 
 	render() {
-		const {  } = this.props;
+		const { src, content, mask } = this.props;
 		return (
-			<div>
-				<div className="hintImg-cover">
-					<div className="hintImg-txt">Here</div>	
-					<img src="https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_960_720.jpg" alt="bg" width="250" height="200" className="hintImg-bg" />
-					
-				</div>
+			<div className="hintImg-cover">
+				<div className="hintImg-txt">{content}</div>	
+				<img src={src} alt="bg" width="250" height="200" className="hintImg-bg" />
 			</div>
 		)
 	}
--- src/HintImg/HintImg.md
+++ src/HintImg/HintImg.md
@@ -1,5 +1,7 @@
 ```jsx
+  const content = <div>Explore</div>;
+
   <div>
-    <HintImg />
+    <HintImg src="https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_960_720.jpg" content={content} />
   </div>
 ```
