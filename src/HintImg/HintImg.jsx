cfb49f46b49d3ec6b7d1bbfc7a9efae7a0b59da1
no-ttt <dear890329@gmail.com>
2022-03-17 13:59:41 +0800

    提示字上升高度&寬高係數調整

--- src/HintImg/HintImg.jsx
+++ src/HintImg/HintImg.jsx
@@ -7,22 +7,31 @@ export class HintImg extends Component {
 	static propTypes = {
 		/** 底層圖片 */
 		src: PropTypes.string.isRequired,
-		/** 內容文字 */
-		content: PropTypes.string,
+		/** 圖片寬度 */
+		width: PropTypes.number,
+		/** 圖片高度 */
+		height: PropTypes.number,
 		/** 控制遮罩 */
 		mask: PropTypes.boolean,
+		/** 內容文字 */
+		content: PropTypes.string,
+		/** 內容文字浮現位置 (數字越大出現位置越下面)*/
+		textPos: PropTypes.number,
 	}
 
 	static defaultProps = {
+		width: 250,
+		height: 200,
 		mask: false,
+		textPos: 0
 	}
 
 	render() {
-		const { src, content, mask } = this.props;
+		const { src, width, height, mask, content, textPos } = this.props;
 		return (
-			<div className={mask ? "hintImg-cover" : "hintImg-no-cover"}>
-				<div className="hintImg-txt">{content}</div>	
-				<img src={src} alt="bg" width="250" height="200" className="hintImg-bg" />
+			<div className={mask ? "hintImg-cover" : "hintImg-no-cover"} style={{width: width, height: height}}>
+				<div className="hintImg-txt" style={{marginTop: textPos}}>{content}</div>
+				<img src={src} alt="bg" className="hintImg-bg" />
 			</div>
 		)
 	}
