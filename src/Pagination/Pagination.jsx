33209b7e047056ff6d2f1c3c53cd72c632cb0130
no-ttt <dear890329@gmail.com>
2022-03-22 17:38:56 +0800

    未完成的分頁

---
+++ src/Pagination/Pagination.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect, Fragment } from 'react';
+import PropTypes from 'prop-types'
+import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft'
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
+
+export const PaginationItem = (props) => {
+	const { color, isCurrent, index, onClick } = props;
+}
---
+++ src/Pagination/Pagination.md
@@ -0,0 +1,12 @@
+```jsx
+    <div style={{ display: "flex", justifyContent: "space-around" }}>
+  <Pagination
+    total={13}
+    withEllipsis={true}
+    ellipsisRange={2}
+    color={"rgb(25, 118, 210)"}
+  />
+</div>
+
+
+```
