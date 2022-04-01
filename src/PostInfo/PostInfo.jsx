f272018b055cc33ff917f7c0ea0477d62b576fcb
no-ttt <dear890329@gmail.com>
2022-04-01 17:22:20 +0800

    景點資訊重新設計

--- src/PostInfo/PostInfo.jsx
+++ src/PostInfo/PostInfo.jsx
@@ -2,13 +2,17 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import CSSModules from 'react-css-modules'
 import styles from '../style/PostInfo.styl'
+import LocationOnIcon from '@material-ui/icons/LocationOn'
+import CallIcon from '@material-ui/icons/Call'
+import ShareIcon from '@material-ui/icons/Share'
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder'
+import FavoriteIcon from '@material-ui/icons/Favorite'
+import { red } from '@material-ui/core/colors'
 
 export class PostInfo extends Component {
 	static propTypes = {
 		/** 景點名稱 */
-		spotName: PropTypes.string.isRequired,
-		/** 簡介 */
-		des: PropTypes.string,
+		spotName: PropTypes.string,
 		/** 電話 */
 		phone: PropTypes.string,
 		/** 地址 */
@@ -29,56 +33,63 @@ export class PostInfo extends Component {
 		posLon: PropTypes.number,
 	}
 
+	constructor(props) {
+		super(props)
+		this.state = {
+			like: false
+		}
+	}
+
 	render() {
-		const { spotName, des, phone, addr, picture, open, ticket, remark, traffic, posLat, posLon } = this.props;
+		const { spotName, phone, addr, picture, open, ticket, remark, traffic, posLat, posLon } = this.props;
 		return (
-			<div className="postInfo-frame">
-				<div className="postInfo-des">
-					<img src={picture} width="500" height="300" className="postInfo-image"></img>
-					<div className="postInfo-des-distance">
-						<div className="postInfo-spotName-txt">{spotName}</div>
-						<table className="postInfo-table">
-							<tbody>
-								<tr>
-									<td valign="top" className="postInfo-td">簡介</td>
-									<td className="postInfo-td-text">{des}</td>
-								</tr>
-								<tr>
-									<td valign="top" className="postInfo-td">電話</td>
-									<td>{phone}</td>
-								</tr>
-								<tr>
-									<td valign="top" className="postInfo-td">地址</td>
-									<td>
-										<a href={"https://www.google.com.tw/maps/@"+posLat.toFixed(7).toString()+","+posLon.toFixed(7).toString()+",15z"} target="_blank" style={{ color: "#d2691e", textDecoration: "none" }}>{addr}</a>
-									</td>
-								</tr>
-							</tbody>
+			<div>
+				<div className="post-info-header">
+					<div className="post-info-spot-name">{spotName}</div>
+					<div className="post-info-header-icon">
+						<button><ShareIcon /></button>
+						<button onClick={() => this.setState({ like: !this.state.like })}>
+							{ !this.state.like
+								? <FavoriteBorderIcon />
+								: <FavoriteIcon style={{ color: red[500] }} />
+							}
+						</button>
+					</div>
+				</div>
+				<div className="post-info-spot-des-block">
+					<div className="post-info-spot-des">
+						<LocationOnIcon />
+						<a href={"https://www.google.com.tw/maps/@"+posLat.toFixed(7).toString()+","+posLon.toFixed(7).toString()+",15z"} target="_blank" style={{ color: "black", textDecoration: "none" }}>{addr}</a>
+					</div>
+					<div className="post-info-spot-des">
+						<CallIcon />
+						<div>{phone}</div>
+					</div>
+				</div>
+				<div className="post-info-detail">
+					<img src={picture} alt={spotName} width={"50%"} className="post-info-img-rwd" />
+					<div className="post-info-tb">
+						<div className="post-info-tb-title">詳細資訊</div>
+						<table>
+							<tr>
+								<td className="post-info-td">開放時間</td>
+								<td className="post-info-text">{open}</td>
+							</tr>
+							<tr>
+								<td className="post-info-td">門票資訊</td>
+								<td className="post-info-text">{ticket}</td>
+							</tr>
+							<tr>
+								<td className="post-info-td">注意事項</td>
+								<td className="post-info-text">{remark}</td>
+							</tr>
+							<tr>
+								<td className="post-info-td">交通方式</td>
+								<td className="post-info-text">{traffic}</td>
+							</tr>
 						</table>
 					</div>
 				</div>
-				<div className="postInfo-hr"></div>
-				<div className="postInfo-other">
-					<table className="postInfo-other-table">
-						<tbody>
-							<tr>
-								<td valign="top" className="postInfo-other-td">開放時間</td>
-								<td>{open}</td>
-							</tr>
-							<tr>
-								<td valign="top" className="postInfo-other-td">門票資訊</td>
-								<td>{ticket}</td>
-							</tr>						<tr>
-								<td valign="top" className="postInfo-other-td">注意事項</td>
-								<td>{remark}</td>
-							</tr>
-							<tr>
-								<td valign="top" className="postInfo-other-td">交通方式</td>
-								<td>{traffic}</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>	
 			</div>
 		)
 	}
