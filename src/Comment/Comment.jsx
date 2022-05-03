3a6f41cc6d3bfe82009f06bee8d092321e0661a3
no-ttt <dear890329@gmail.com>
2022-05-03 14:23:03 +0800

    增加旅遊小幫手模式

--- src/Comment/Comment.jsx
+++ src/Comment/Comment.jsx
@@ -6,22 +6,39 @@ import Avatar from '../Avatar/Avatar'
 import Rating from '../Rating/Rating'
 import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt'
 import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt'
+import PushPinIcon from '@mui/icons-material/PushPin'
 import MoreVertIcon from '@mui/icons-material/MoreVert'
 
 export class Comment extends Component {
 	static propTypes = {
-
+		/** 使用者名稱 */
+		userName: PropTypes.string,
+		/** 按讚數量 */
+		like: PropTypes.number,
+		/** 評分 */
+		rating: PropTypes.number,
+		/** 評論 */
+		comment: PropTypes.string,
+		/** 照片 */
+		img: PropTypes.array,
+		/** 發布日期 */
+		date: PropTypes.string,
+		/** 景點描述 */
+		des: PropTypes.string,
+		/** 模式 (comment or direct) */
+		mode: PropTypes.string,
 	}
 
 	static defaultProps = {
-
+		like: 0,
+		mode: "comment",
 	}
 
 	constructor(props) {
 		super(props);
 		this.state = {
 			click: false,
-			clickNum: 100,
+			clickNum: this.props.like,
 		}
 		this.isClick = this.isClick.bind(this);
 	}
@@ -40,37 +57,53 @@ export class Comment extends Component {
 	}
 
 	render() {
-		const { src, userName } = this.props;
-		return (
-			<div>
-				<div className="comment-header">
-					<div className="comment-title">
-						<Avatar alt="user_avatar" src={src} width={45} />
-						<div>
+		const { src, userName, rating, content, img, date, mode, des } = this.props;
+		if (mode === "comment")
+			return (
+				<div>
+					<div className="comment-header">
+						<div className="comment-title">
+							<Avatar alt="user_avatar" src={src} width={45} />
 							<div className="comment-user-name">{userName}</div>
 						</div>
+						<div className="comment-thumbup">
+							<button onClick={this.isClick}>
+								{ this.state.click ? <ThumbUpAltIcon /> : <ThumbUpOffAltIcon /> }
+							</button>
+							<div style={{ fontSize: "14px" }}>{this.state.clickNum}</div>
+							{/* <button><MoreVertIcon /></button> */}
+						</div>
 					</div>
-					<div className="comment-thumbup">
-						<button onClick={this.isClick}>
-							{ this.state.click ? <ThumbUpAltIcon /> : <ThumbUpOffAltIcon /> }
-						</button>
-						<div style={{ fontSize: "14px" }}>{this.state.clickNum}</div>
-						{/* <button><MoreVertIcon /></button> */}
+					<div className="comment-content">
+						<Rating size="small" fixed={true} />
+						<div>{content}</div>
+					</div>
+					<div className="comment-img-date">
+						<div className="comment-img-section">
+							{img.map((src, i) =>
+								<img key={i} src={src} alt={"photo-" + i} className="comment-photo" />
+							)}
+						</div>
+						<div className="comment-date">{date}</div> 
 					</div>
 				</div>
-				<div className="comment-content">
-					<Rating size="small" />
-					<div>適合假日走走放鬆心情！</div>
-				</div>
-				<div className="comment-img-date">
-					<div className="comment-img-section">
-						<img src="https://cdn2.ettoday.net/images/4995/d4995352.jpg" alt="photo-1" className="comment-photo" />
-						<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYmj3zOgrQlhp5OpzaUf_fmvwi0XbZ61_DcA&usqp=CAU" alt="photo-2" className="comment-photo" />
+			)
+		if (mode === "direct")
+			return (
+				<div>
+					<div className="comment-header">
+						<div className="comment-title">
+							<Avatar alt="user_avatar" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7A5w9fXNr22x_vZzJ17ubf2X2VX7ZSzBeMQ&usqp=CAU" width={45} />
+							<div className="comment-user-name">旅遊小幫手</div>
+						</div>
+						<div className="comment-pin">
+							<PushPinIcon sx={{ fontSize: 15 }} />
+							<div style={{ fontSize: "12px" }}>置頂</div>
+						</div>
 					</div>
-					<div className="comment-date">2022年1月1日</div> 
+					<div className="comment-content">{des}</div>
 				</div>
-			</div>
-		)
+			)
 	}
 }
 
--- src/Comment/Comment.md
+++ src/Comment/Comment.md
@@ -1,8 +1,24 @@
 ```jsx
+const comment = {
+  src: "https://p3-tt.byteimg.com/origin/pgc-image/def93625146b46f8980a39f6e0057b2f?from=pc",
+  userName: "test",
+  like: 100,
+  rating: 3,
+  content: "適合假日走走放鬆心情！",
+  img: ["https://cdn2.ettoday.net/images/4995/d4995352.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYmj3zOgrQlhp5OpzaUf_fmvwi0XbZ61_DcA&usqp=CAU"],
+  date: "2022年1月1日",
+};
+
 <div style={{ width: "70%" }}>
-  <Comment 
-    src="https://p3-tt.byteimg.com/origin/pgc-image/def93625146b46f8980a39f6e0057b2f?from=pc"
-    userName="test"
+  <Comment mode="comment" src={comment.src} userName={comment.userName} like={comment.like} rating={comment.rating} content={comment.content} img={comment.img} date={comment.date}
   />
+</div>
+```
+### Direct
+```jsx
+const des= "紫坪位在綠島最南方，緊鄰「綠島露營區」。從露營區旁的步道，可通往海岸邊的潟湖「紫坪」。「紫坪」是一處由珊瑚礁構成的潮池，也是綠島著名的潟湖所在地，有全綠島最完整的潟湖地形以及珊瑚礁植群，更有茂盛的植物水芫花和珍貴的陸寄居蟹。外海儘管浪濤洶湧，內湖依然波平如鏡，宛若沉睡的湖水，清淺的躺在外珊瑚礁岩與內珊瑚貝砂灘間；水芫花灌叢身影倒映於平靜無波的水面上，潔白柔細的白砂鋪陳水底。熱帶海岸旖旎風情，盡在不言中。";
+
+<div style={{ width: "70%" }}>
+  <Comment mode="direct" des={des} />
 </div>
 ```
