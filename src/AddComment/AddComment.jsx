a174342202d344af5084001bd1f6cc375eb9c8c7
no-ttt <dear890329@gmail.com>
2022-05-04 12:05:42 +0800

    添加留言的彈跳窗（上傳圖片功能尚未完成）

--- src/AddComment/AddComment.jsx
+++ src/AddComment/AddComment.jsx
@@ -2,28 +2,53 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import CSSModules from 'react-css-modules'
 import styles from '../style/AddComment.styl'
+import Popup from '../PoPup/Popup'
 import Avatar from '../Avatar/Avatar'
 import Rating from '../Rating/Rating'
+import AddAPhotoIcon from '@mui/icons-material/AddAPhoto'
 
 export class AddComment extends Component {
 	static propTypes = {
 		/** 觸發元件 */
 		clickBtn: PropTypes.string,
-    }
+
+	}
 
 	static defaultProps = {
 	}
 
+	constructor(props) {
+		super(props);
+		this.state =  {
+			rating: 0,
+			comment: "",
+		}
+	}
+
+	sendComment = () => {
+		this.props.returnComment(this.state.rating, this.state.comment)
+	} 
+
 	render() {
 		const { clickBtn, title, src, userName } = this.props;
 		return (
-			<div>
-				<div className="add-comment-user-info">
-					<Avatar alt="user_avatar" src={src} width={45} />
-					<div class="add-comment-user">{userName}</div>
+			<Popup clickBtn={clickBtn} width={400} height={480} title={title}>
+				<div>
+					<div className="add-comment-user-info">
+						<Avatar alt="user_avatar" src={src} width={45} />
+						<div class="add-comment-user">{userName}</div>
+					</div>
+					<div className="add-comment-section">
+						<Rating max={5} fixed={false} size="large" value={(n) => this.setState({ rating: n })} />
+						<textarea id="comment" name="comment" rows="8" placeholder="寫些什麼？" 
+							className="add-comment-textarea" onChange={e => this.setState({ comment: e.target.value })} />
+						<div className="add-comment-upload-img">
+							<AddAPhotoIcon sx={{ fontSize: 30 }} />
+						</div>
+						<button className="add-comment-btn" onClick={this.sendComment}>我要發佈</button>
+					</div>
 				</div>
-				<Rating />
-			</div>
+			</Popup>
 		)
 	}
 }
--- src/AddComment/AddComment.md
+++ src/AddComment/AddComment.md
@@ -1,9 +1,9 @@
 ```jsx
-	const clickBtn = <div>Click Me !</div>;
-	const userInfo = {
+  const clickBtn = <div>Click Me !</div>;
+  const userInfo = {
     src: "https://p3-tt.byteimg.com/origin/pgc-image/def93625146b46f8980a39f6e0057b2f?from=pc",
     userName: "test",
   };
 
-	<AddComment clickBtn={clickBtn} title="日月潭" src={userInfo.src} userName={userInfo.userName} />
+  <AddComment clickBtn={clickBtn} title="日月潭" src={userInfo.src} userName={userInfo.userName} returnComment={(rating, comment) => {alert("評分：" + rating + "\n評論：" + comment)}} />
 ```
